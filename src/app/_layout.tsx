53bcbcb6f2c3a398173fc1ae3cd1569f2c0a6318
sanha <heftycornerstone@gmail.com>
2025-02-05 17:03:42 +0900

    feat: 스플래시 스크린, 어플 아이콘 추가, 스플래시 스크린 효과 적용

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,28 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.setOptions({
+  duration: 300,
+  fade: true,
+});
+
+// keep splash screen visible 
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  useEffect(() => {
+    const prepare = async () => {
+      
+      // pre-load
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+
+      // hide splash screen
+      await SplashScreen.hideAsync();
+    };
+    prepare();
+  }, []);
+
   return <Stack />;
 }
+
